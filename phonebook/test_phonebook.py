8118a0b0cb51fcae27718f9d658f99a2fc9fd80b
Chris Aporta <christopher.aporta@gmail.com>
2017-08-14 01:31:37 -0400

    Introduce test case design pattern

--- phonebook/test_phonebook.py
+++ phonebook/test_phonebook.py
@@ -14,6 +14,30 @@ class PhonebookTest(unittest.TestCase):
         with self.assertRaises(KeyError):
             self.phonebook.lookup('missing')
 
-    @unittest.skip('WIP')
     def test_empty_phonebook_is_consistent(self):
         self.assertTrue(self.phonebook.is_consistent())
+
+    def test_phonebook_with_normal_entries_is_consistent(self):
+        self.phonebook.add('Bob', '12345')
+        self.phonebook.add('Mary', '012345')
+        self.assertTrue(self.phonebook.is_consistent())
+
+    def test_phonebook_with_duplicate_entries_is_inconsistent(self):
+        self.phonebook.add('Bob', '12345')
+        self.phonebook.add('Mary', '12345')
+        self.assertFalse(self.phonebook.is_consistent())
+
+    def test_phonebook_with_a_number_that_prefixes_another_is_inconsistent(self):
+        self.phonebook.add('Bob', '12345')
+        self.phonebook.add('Mary', '123')
+        self.assertFalse(self.phonebook.is_consistent())
+
+    @unittest.skip('WIP')
+    def test_phonebook_adds_names(self):
+        self.phonebook.add('Sue', '12345')
+        self.assertIn('Sue', phonebook.get_names())
+
+    @unittest.skip('WIP')
+    def test_phonebook_adds_numbers(self):
+        self.phonebook.add('Sue', '12345')
+        self.assertIn('12345', phonebook.get_numbers())
