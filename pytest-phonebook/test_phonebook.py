3aa6bf973829a3f0956c2407108b10e91a9c6616
Chris Aporta <christopher.aporta@gmail.com>
2017-08-21 22:58:56 -0400

    Leverage dependency injection via pytest fixtures

--- pytest-phonebook/test_phonebook.py
+++ pytest-phonebook/test_phonebook.py
@@ -3,25 +3,29 @@ import pytest
 from phonebook import Phonebook
 
 
-def test_add_and_lookup_entry():
-    phonebook = Phonebook()
+@pytest.fixture
+def phonebook():
+    return Phonebook()
+
+
+def test_add_and_lookup_entry(phonebook):
     phonebook.add('Bob', '123')
     assert '123' == phonebook.lookup('Bob')
 
 
-def test_phonebook_gives_access_to_names():
+def test_phonebook_gives_access_to_names(phonebook):
     phonebook = Phonebook()
     phonebook.add('Alice', '12345')
     assert 'Alice' in phonebook.names()
 
 
-def test_phonebook_gives_access_to_numbers():
+def test_phonebook_gives_access_to_numbers(phonebook):
     phonebook = Phonebook()
     phonebook.add('Alice', '12345')
     assert '12345' in phonebook.numbers()
 
 
-def test_missing_entry_raises_KeyError():
+def test_missing_entry_raises_KeyError(phonebook):
     phonebook = Phonebook()
     with pytest.raises(KeyError):
         phonebook.lookup('missing')
