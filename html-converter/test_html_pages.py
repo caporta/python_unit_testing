85ad9e657b5782a074859bde125b969c40d8f6c2
caporta <caporta@stridenyc.com>
2017-08-23 15:01:53 -0400

    Build naive implementation of HtmlPagesConverter + spec

---
+++ html-converter/test_html_pages.py
@@ -0,0 +1,16 @@
+import unittest
+import os
+import tempfile
+
+from .html_pages import HtmlPagesConverter
+
+
+class HtmlPagesTest(unittest.TestCase):
+    def test_inserts_br_tags_for_linebreaks(self):
+        filename = os.path.join(tempfile.gettempdir(), 'afile.txt')
+        f = open(filename, 'w', encoding='utf-8')
+        f.write('plain text\n')
+        f.close()
+        converter = HtmlPagesConverter(filename)
+        new_text = converter.get_html_page(0)
+        self.assertEqual('plain text<br />', new_text)
